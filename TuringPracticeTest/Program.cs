37a5427d794269b8d1c3648f23e46672d0546812
FabianRolon <fabi18fr@gmail.com>
2022-03-05 14:28:38 -0300

    Primer commit

---
+++ TuringPracticeTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuringPracticeTest
+{
+    class Solution
+    {
+        public int CalPoints(string[] ops)
+        {
+            
+            return int.Parse(ops[0]);
+        }
+    }
+
+    class CalPoints
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            char[] space = new char[] { };
+
+            string[] ops = Console.ReadLine().Split(space);
+
+            //int output = solution.CalPoints(ops);
+
+            //Console.WriteLine(output.ToString());
+            foreach(var a in ops)
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
